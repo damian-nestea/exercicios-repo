9679cb5b21b360713ecb58dad826bee5183200ed
Damian Martinez <damian@hotmail.cz>
2023-02-07 21:12:27 -0300

    feature resolução dos exercícios usando useState

--- react/imutabilidade-e-useState-exercicios-main/src/App.js
+++ react/imutabilidade-e-useState-exercicios-main/src/App.js
@@ -14,76 +14,91 @@ const FlexContainer = styled.div`
   flex-wrap: wrap;
 `
 function App() {
-  
-
   // Para fazer seus próximos pokemons, crie novos estados ultilizando como referência o objeto abaixo:
-  const [pokemon , setPokemon] = useState({
-    name: "Pichu",
-    type: "Electric",
+  const [pokemon , setPokemon] = useState([
+    {
+      name: "Pichu",
+      type: "Electric",
+      evolved: false,
+      weight: 2,
+      color: 'yellow',
+      image: 'https://archives.bulbagarden.net/media/upload/archive/f/f3/20130810070434%210172Pichu.png',
+      id: 0
+   },
+   {
+    name: "Bulbasaur",
+    type: "Grass-Poison",
     evolved: false,
     weight: 2,
+    color: 'green',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/250px-0001Bulbasaur.png',
+    id: 1
+  },
+  {
+    name: "Charmander",
+    type: "Fire",
+    evolved: false,
+    weight: 8.5,
+    color: 'orange',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/2/27/0004Charmander.png/250px-0004Charmander.png',
+    id: 2
+  },
+  {
+    name: "Psyduck",
+    type: "Water",
+    evolved: false,
+    weight: 19.6,
+    color: 'lightblue',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/3/3f/0054Psyduck.png/250px-0054Psyduck.png',
+    id: 3
+  },
+]);
+
+const pokemonEvoluido =[
+  {
+    name: "Pikachu",
+    type: "Electric",
+    evolved: true,
+    weight: 6,
     color: 'yellow',
     image: 'https://archives.bulbagarden.net/media/upload/archive/f/f3/20130810070434%210172Pichu.png',
     id: 0
-  });
-
-  const pichuEvolved = {
-      name: "Pikachu",
-      type: "Electric",
-      evolved: true,
-      weight: 6,
-      color: 'yellow',
-      image: 'https://archives.bulbagarden.net/media/upload/thumb/4/4a/0025Pikachu.png/250px-0025Pikachu.png',
-      id: 1
-    }
-    
-    const [pokemon1 , setPokemon1] = useState({
-      name: "Bulbasaur",
-      type: "Grass-Poison",
-      evolved: false,
-      weight: 6.9,
-      color: 'green',
-      image: 'https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/250px-0001Bulbasaur.png',
-      id: 2
-    });
-  
-    const bulbasaurEvolved = {
-        name: "Ivysaur",
-        type: "Grass-Poison",
-        evolved: true,
-        weight: 13,
-        color: 'green',
-        image: 'https://archives.bulbagarden.net/media/upload/thumb/8/81/0002Ivysaur.png/250px-0002Ivysaur.png',
-        id: 4
-      }
+ },
+ {
+  name: "Ivysaur",
+  type: "Grass-Poison",
+  evolved: true,
+  weight: 6,
+  color: 'green',
+  image: 'https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/250px-0001Bulbasaur.png',
+  id: 1
+},
+{
+  name: "Charmeleon",
+  type: "Fire",
+  evolved: true,
+  weight: 12.5,
+  color: 'orange',
+  image: 'https://archives.bulbagarden.net/media/upload/thumb/2/27/0004Charmander.png/250px-0004Charmander.png',
+  id: 2
+},
+{
+  name: "Golduck",
+  type: "Water",
+  evolved: true,
+  weight: 29.6,
+  color: 'lightblue',
+  image: 'https://archives.bulbagarden.net/media/upload/thumb/3/3f/0054Psyduck.png/250px-0054Psyduck.png',
+  id: 3
+}
+];
 
 
   return ( <>
   <GlobalStyles/>
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard
-        nome = {pokemon.name}
-        type = {pokemon.type}
-        evolved = {pokemon.evolved}
-        weight = {pokemon.weight}
-        color = {pokemon.color}
-        image = {pokemon.image}
-        id = {pokemon.id}
-        setPokemon = {setPokemon}
-        pokemonEvolved = {pichuEvolved}
-      />
-      <PokemonCard
-        nome = {pokemon1.name}
-        type = {pokemon1.type}
-        evolved = {pokemon1.evolved}
-        weight = {pokemon1.weight}
-        color = {pokemon1.color}
-        image = {pokemon1.image}
-        id = {pokemon1.id}
-        setPokemon = {setPokemon1}
-        pokemonEvolved = {bulbasaurEvolved}
-      />
+      <PokemonCard pokemons = {pokemon} setPokemon = {setPokemon} pokemonEvoluido ={pokemonEvoluido}/>
     </FlexContainer>
   </>
     
--- react/imutabilidade-e-useState-exercicios-main/src/Components/PokemonCard/PokemonCard.js
+++ react/imutabilidade-e-useState-exercicios-main/src/Components/PokemonCard/PokemonCard.js
@@ -1,24 +1,44 @@
 import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton , Imagem} from './styles'
+import { Titulo, CardContainer ,Card, PokemonName, PokemonType, EvolveButton , Imagem} from './styles'
 
 
 
-const PokemonCard = (props) => {
+const PokemonCard = ({pokemons, setPokemon, pokemonEvoluido}) => {
 
-    const evoluirPokemon = () => {
-      props.setPokemon(props.pokemonEvolved);
+    const evoluirPokemon = (id,evoluido) => {
+      if(!evoluido){
+        const pokemonAEvoluir = pokemonEvoluido.find((pokemon)=>{
+          return pokemon.id === id;
+        })
+        
+        const pokemonEvolved = pokemons.map((item) =>{
+          if(item.id === pokemonAEvoluir.id){
+            return {
+              ...item, name: pokemonAEvoluir.name, color: pokemonAEvoluir.color, image: pokemonAEvoluir.image, type: pokemonAEvoluir.type, weight: pokemonAEvoluir.weight, evolved: pokemonAEvoluir.evolved
+            }
+          }
+          return item;
+        })
+        setPokemon(pokemonEvolved); 
+      }else{
+        alert("Pokemon já evoluído!");
+      }
     }
     
   return (
-    <Card color={props.color}>
-        <Imagem src={props.image} alt={`Pokemon`}/>
-        <PokemonName>{props.nome}</PokemonName>
-        <PokemonType>{props.type}</PokemonType>
-        <p>{props.weight}kg</p>
+    <CardContainer>
+      <Titulo>POKEMONS</Titulo>
+      {pokemons.map((pokemon) => (
+      <Card key={pokemon.color} color={pokemon.color}>
+          <Imagem key={pokemon.image} src={pokemon.image} alt={`Pokemon`}/>
+          <PokemonName key={pokemon.name}>{pokemon.name}</PokemonName>
+          <PokemonType key={pokemon.type}>{pokemon.type}</PokemonType>
+          <p key={pokemon.weight}>{pokemon.weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
-
-    </Card>
+          <EvolveButton onClick={() => evoluirPokemon(pokemon.id,pokemon.evolved)}>Evoluir!</EvolveButton>
+      </Card>
+      ))}
+    </CardContainer>
   )
 }
 
